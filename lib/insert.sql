07d32123b673a74d7a04ece9f2c36a4e66153d61
lucasfarmwald <farmwaldlucas@gmail.com>
2022-04-11 11:08:38 -0500

    final commit

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1, 'Mr. Chocolate', 10, 'Male', 'Brown', 'Calm', 1);
+INSERT INTO bears VALUES (2, 'Rowdy', 8, 'Male', 'Pink', 'Calm', 1);
+INSERT INTO bears VALUES (3, 'Tabitha', 6, 'F', 'Brown', 'Calm', 1);
+INSERT INTO bears VALUES (4, 'Sergeant Brown', 9, 'Male', 'Brown', 'Calm', 1);
+INSERT INTO bears VALUES (5, 'Melissa', 10, 'F', 'Blue', 'Calm', 1);
+INSERT INTO bears VALUES (6, 'Grinch', 5, 'Female', 'Brown', 'Agressive', 1);
+INSERT INTO bears VALUES (7, 'Wendy', 2, 'F', 'Red', 'Calm', 1);
+INSERT INTO bears VALUES (8, null, 14, 'Male', 'Brown', 'Happy', 1);
